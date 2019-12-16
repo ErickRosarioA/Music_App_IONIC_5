dd45a6c695d4d265ed78c8ac7d26b8725ebf1e2c
Erick Rosario <erick_rosario@claro.com.do>
2019-12-16 16:23:20 -0400

    Login integrated component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  constructor(public actionRegisterType:ActionSheetController) {}
+  async clickRegister(){
+    const actionSheet = await this.actionRegisterType.create({
+      header: 'Register Options', 
+      buttons: [{
+        text: 'Gmail',
+        role: 'destructive',
+        icon: 'logo-google',
+        handler: () => {
+          console.log('Funtion  clicked');
+        }
+      }, {
+        text: 'Facebook',
+        icon: 'logo-facebook',
+        handler: () => {
+          console.log('Funtion clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
